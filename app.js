994ebe4868df32cea1eeb7b959871c960126f35a
ysenpi <ilovesigna@gmail.com>
2021-07-17 19:48:17 +0300

    fix bugs

--- app.js
+++ app.js
@@ -8,54 +8,55 @@ app.use(express.json({limit: '30mb', extended: true}))
 app.use(express.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
 
-let tasks = []
-const profile = {
-  name: '🐺👆',
-  username: 'auf.6.9',
-  avatar: 'https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/fantastic-mr-fox-wolf-teassa-herdian.jpg'
-}
+let dbTasks = []
 
 app.get('/', (_, res) => {
   res.send('hello gusi')
 })
 
 app.get('/tasks', (_, res) => {
+  const tasks = dbTasks.filter(task => task.completed === false)
+
   res.status(200).json(tasks)
 })
 
 app.post('/orders', (req, res) => {
   const { username } = req.body
-
-  console.log(username)
-
-  const orders = tasks.filter(task => task.username === username)
+  const orders = dbTasks.filter(task => task.username === username)
 
   res.status(200).json(orders)
 })
 
 app.get('/profile', (_, res) => {
-  res.status(200).json(profile)
+  res.status(200).json({})
 })
 
 app.post('/tasks/create', (req, res) => {
-  console.log(req.body)
   const { id, username, url, likes } = req.body
 
-  tasks.push({id, username, url, likes})
+  dbTasks.push({id, username, url, likes, whoLiked: [], completed: false})
 
-  res.status(201).json({result: 'created', tasks})
+  res.status(201).json({result: 'created'})
 })
 
-app.post('/tasks/:id/like', (req, res) => {
+app.post('/tasks/like', (req, res) => {
+  const { id, username } = req.body 
+  const index = dbTasks.findIndex(task => task.id === id)
+  
+  dbTasks[index].whoLiked.includes(username) || dbTasks[index].whoLiked.push(username)
+  dbTasks[index].whoLiked.length >= dbTasks[index].likes ? dbTasks[index].completed = true : null
+  
+  console.log(dbTasks)
+
   res.status(200).json({result: 'liked'})
 })
 
-app.post('/tasks/like', (_, res) => {
-  res.status(200).json({result: 'liked'})
-})
+// app.post('/tasks/:id/like', (req, res) => {
+//   res.status(200).json({result: 'liked'})
+// })
 
-app.post('/tasks/delete', (_, res) => {
-  res.status(200).json({result: 'deleted'})
-})
+// app.post('/tasks/delete', (_, res) => {
+//   res.status(200).json({result: 'deleted'})
+// })
 
 app.listen(PORT, () => console.log(`server on ${PORT}..`))
